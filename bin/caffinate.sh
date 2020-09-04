6004f8109c64f68e3c5aa555ebd7247dd23d1ad2
Richard Alex Hofer <rhofer@alumni.cmu.edu>
2020-09-04 09:50:44 -0400

    Add scripts to enable/disable sleep.

---
+++ bin/caffinate.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+gsettings set org.gnome.settings-daemon.plugins.power idle-dim false
+gsettings set org.gnome.desktop.session idle-delay 0
---
+++ bin/decaffinate.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+gsettings set org.gnome.settings-daemon.plugins.power idle-dim true
+gsettings set org.gnome.desktop.session idle-delay 600
